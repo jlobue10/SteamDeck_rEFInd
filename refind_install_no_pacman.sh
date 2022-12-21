56e740e2d13c8df1b44f8c0f1dac9dc09f7ea0b3
jlobue10 <jlobue10@gmail.com>
2022-12-21 07:43:03 -0800

    Optional no Pacman installation script
    In case pacman has an issue getting and installing the refind package, this installer should work by manually downloading (from sourceforge) and installing.

---
+++ refind_install_no_pacman.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# An alternate,  without pacman, rEFInd installation
+# Please make sure that a password exists for the deck user before running
+
+cd ~/Downloads
+wget https://sourceforge.net/projects/refind/files/0.13.3.1/refind-bin-gnuefi-0.13.3.1.zip
+unzip -a refind-bin-gnuefi-0.13.3.1.zip
+
+sudo steamos-readonly disable
+sudo mkdir -p /esp/EFI/refind
+yes | sudo cp ~/Downloads/refind-bin-0.13.3.1/refind/refind_x64.efi /esp/EFI/refind/
+yes | sudo cp -rf ~/Downloads/refind-bin-0.13.3.1/refind/drivers_x64/ /esp/efi/refind
+yes | sudo cp -rf ~/Downloads/refind-bin-0.13.3.1/refind/tools_x64/ /esp/efi/refind
+sudo ./refind-bin-0.13.3.1/refind-install
+yes | sudo cp -rf ~/Downloads/refind-bin-0.13.3.1/refind/icons/ /esp/efi/refind
+yes | sudo cp -rf ~/Downloads/refind-bin-0.13.3.1/fonts/ /esp/efi/refind
+
+efibootmgr | tee ~/efibootlist.txt
+grep -A0 'Windows' ~/efibootlist.txt | tee ~/windows_boot.txt
+WINDOWS_BOOTNUM="$(grep -Eo '[0-9]{1,}' ~/windows_boot.txt)"
+# Disable Windows EFI boot entry
+sudo efibootmgr -b $WINDOWS_BOOTNUM -A
+grep -A0 'rEFInd Boot Manager' ~/efibootlist.txt | tee ~/rEFInd_boot.txt
+REFIND_BOOTNUM="$(grep -Eo '[0-9]{1,}' ~/rEFInd_boot.txt)"
+# Delete rEFInd EFI boot entry from rEFInd-install... will be re-added later pointing to esp partition
+sudo efibootmgr -b $REFIND_BOOTNUM -B
+# Checking for duplicate rEFInd EFI boot entry, from previous script runs (or other sources)
+efibootmgr | tee ~/efibootlist2.txt
+grep -A0 'rEFInd' ~/efibootlist2.txt | tee ~/rEFInd_boot2.txt
+REFIND_BOOTNUM_ALT="$(grep -Eo '[0-9]{1,}' ~/rEFInd_boot2.txt)"
+
+# Deleting duplicate rEFInd boot entry, if one was found
+re='^[0-9]+$'
+if [[ $REFIND_BOOTNUM_ALT =~ $re ]]; then
+	sudo efibootmgr -b $REFIND_BOOTNUM_ALT -B
+fi
+
+git clone https://github.com/jlobue10/SteamDeck_rEFInd/
+cd ~/Downloads/SteamDeck_rEFInd
+CURRENT_WD=$(pwd)
+yes | sudo cp $CURRENT_WD/refind.conf /esp/efi/refind/refind.conf
+yes | sudo cp -rf $CURRENT_WD/themes/ /esp/efi/refind
+yes | sudo cp -rf $CURRENT_WD/icons/ /esp/efi/refind
+sudo efibootmgr -c -d /dev/nvme0n1 -p 1 -L "rEFInd" -l \\efi\\refind\\refind_x64.efi
+
+# Clean up temporary files
+yes | rm ~/efibootlist.txt
+yes | rm ~/efibootlist2.txt
+yes | rm ~/windows_boot.txt
+yes | rm ~/rEFInd_boot.txt
+yes | rm ~/rEFInd_boot2.txt
+
+sudo steamos-readonly enable
+echo "rEFInd has now been installed, without pacman."
