d1271f18118ffb18e3a77aa2609a9770059095cc
jlobue10 <jlobue10@gmail.com>
2023-01-02 15:38:15 -0800

    Code clean up and simplification
    Same functionality as before with fewer lines

--- refind_install_no_pacman.sh
+++ refind_install_no_pacman.sh
@@ -20,20 +20,15 @@ yes | sudo cp -rf $CURRENT_WD/themes/ /esp/efi/refind
 yes | sudo cp -rf $CURRENT_WD/icons/ /esp/efi/refind
 
 efibootmgr | tee ~/efibootlist.txt
-grep -A0 'Windows' ~/efibootlist.txt | tee ~/windows_boot.txt
-WINDOWS_BOOTNUM="$(grep -Eo '[0-9]{1,}' ~/windows_boot.txt | head -1)"
+WINDOWS_BOOTNUM="$(grep -A0 'Windows' ~/efibootlist.txt | grep -Eo '[0-9]{1,4}' | head -1)"
 # Disable Windows EFI boot entry
 sudo efibootmgr -b $WINDOWS_BOOTNUM -A
-grep -A0 'rEFInd Boot Manager' ~/efibootlist.txt | tee ~/rEFInd_boot.txt
-REFIND_BOOTNUM="$(grep -Eo '[0-9]{1,}' ~/rEFInd_boot.txt | head -1)"
+REFIND_BOOTNUM="$(grep -A0 'rEFInd Boot Manager' ~/efibootlist.txt | grep -Eo '[0-9]{1,4}' | head -1)"
 # Delete rEFInd EFI boot entry from rEFInd-install... will be re-added later pointing to esp partition
 sudo efibootmgr -b $REFIND_BOOTNUM -B
 # Checking for duplicate rEFInd EFI boot entry, from previous script runs (or other sources)
-efibootmgr | tee ~/efibootlist2.txt
-grep -A0 'rEFInd' ~/efibootlist2.txt | tee ~/rEFInd_boot2.txt
-REFIND_BOOTNUM_ALT="$(grep -Eo '[0-9]{1,}' ~/rEFInd_boot2.txt | head -1)"
-grep -A0 'SteamOS' ~/efibootlist2.txt | tee ~/SteamOS_boot.txt
-STEAMOS_BOOTNUM="$(grep -Eo '[0-9]{1,}' ~/SteamOS_boot.txt | head -1)"
+REFIND_BOOTNUM_ALT="$(grep -A0 'rEFInd' ~/efibootlist.txt | grep -Eo '[0-9]{1,4}' | head -1)"
+STEAMOS_BOOTNUM="$(grep -A0 'SteamOS' ~/efibootlist.txt | grep -Eo '[0-9]{1,4}' | head -1)"
 
 # Deleting duplicate rEFInd boot entry, if one was found
 re='^[0-9]+$'
@@ -54,14 +49,8 @@ sudo efibootmgr -c -d /dev/nvme0n1 -p 1 -L "rEFInd" -l \\EFI\\refind\\refind_x64
 yes | sudo cp $CURRENT_WD/bootnext-refind.service /etc/systemd/system/bootnext-refind.service
 sudo systemctl enable --now bootnext-refind.service
 
-# Clean up temporary files, created for code clarity and readability
-# Used primarily in automatically discovering boot entry numbers for efibootmgr commands
+# Clean up temporary files, created for code clarity
 yes | rm ~/efibootlist.txt
-yes | rm ~/efibootlist2.txt
-yes | rm ~/windows_boot.txt
-yes | rm ~/rEFInd_boot.txt
-yes | rm ~/rEFInd_boot2.txt
-yes | rm ~/SteamOS_boot.txt
 
 sudo steamos-readonly enable
 echo -e "\nrEFInd has now been installed, without pacman."
