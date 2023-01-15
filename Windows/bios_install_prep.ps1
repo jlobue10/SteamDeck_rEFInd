7184596b97e93bff61c5b63f823fcc18fc684368
jlobue10 <jlobue10@gmail.com>
2023-01-15 15:26:59 -0800

    Simplified rEFInd GUID discovery
    Accomplishes the same thing as before (still language independent), but with fewer steps.

--- Windows/bios_install_prep.ps1
+++ Windows/bios_install_prep.ps1
@@ -1,10 +1,7 @@
 # Run this to remove the rEFInd EFI entry as boot next EFI entry (bootsequence first)
 # Be sure to also disable the scheduled task enabling rEFInd as first in the bootsequence
 
-$BCD_INFO = $(bcdedit /enum FIRMWARE)
-$FILE_IDX = ($BCD_INFO | Select-String 'refind_x64.efi').LineNumber
-$SEPARATORS = ($BCD_INFO | Select-String '--' ).LineNumber
-foreach ($_ in $SEPARATORS) { if ($_ -lt $FILE_IDX) { $GUID_IDX = $_ } }
-$REFIND_GUID = ($BCD_INFO | Select-Object -Index $GUID_IDX | Select-String "{.*}").Matches.Value
+$REFIND_IDENT = bcdedit /enum FIRMWARE | Select-String -Pattern 'refind_x64.efi' -Context 2 | findstr "{.*}"
+$REFIND_GUID = ($REFIND_IDENT -split ' ')[-1]
 
 bcdedit /set "{fwbootmgr}" bootsequence "$REFIND_GUID" /remove
