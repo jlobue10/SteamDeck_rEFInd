bd66345a06ca4287a912b861374ce8e6ff3d72de
jlobue10 <jlobue10@gmail.com>
2023-01-15 18:43:44 -0800

    Syntax and code update

--- Windows/bios_install_prep.ps1
+++ Windows/bios_install_prep.ps1
@@ -1,7 +1,7 @@
 # Run this to remove the rEFInd EFI entry as boot next EFI entry (bootsequence first)
 # Be sure to also disable the scheduled task enabling rEFInd as first in the bootsequence
 
-$REFIND_IDENT = bcdedit /enum FIRMWARE | Select-String -Pattern 'refind_x64.efi' -Context 2 | findstr "{.*}"
-$REFIND_GUID = ($REFIND_IDENT -split ' ')[-1]
+$REFIND_IDENT = bcdedit /enum FIRMWARE | Select-String -Pattern 'refind_x64.efi' -Context 2 | findstr "{"
+$REFIND_GUID = ($REFIND_IDENT | Select-String "{.*}").Matches.Value
 
 bcdedit /set "{fwbootmgr}" bootsequence "$REFIND_GUID" /remove
