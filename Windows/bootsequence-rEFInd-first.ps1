138c2a9b247211dd5b3c1c2caeb266e7ec45e637
jlobue10 <jlobue10@gmail.com>
2023-01-15 18:42:53 -0800

    Syntax and code update

--- Windows/bootsequence-rEFInd-first.ps1
+++ Windows/bootsequence-rEFInd-first.ps1
@@ -1,8 +1,8 @@
 # Setting next boot as rEFInd... Windows PowerShell script (Run as Administrator) for task scheduler
 # Credit goes to Reddit user lucidludic for the idea and some code snippets (modified)
 
-$REFIND_IDENT = bcdedit /enum FIRMWARE | Select-String -Pattern 'refind_x64.efi' -Context 2 | findstr "{.*}"
-$REFIND_GUID = ($REFIND_IDENT -split ' ')[-1]
+$REFIND_IDENT = bcdedit /enum FIRMWARE | Select-String -Pattern 'refind_x64.efi' -Context 2 | findstr "{"
+$REFIND_GUID = ($REFIND_IDENT | Select-String "{.*}").Matches.Value
 
 bcdedit /set "{fwbootmgr}" bootsequence "$REFIND_GUID" /addfirst
 
