d45644d36a942d5120684d871f7a30a2d97894e5
jlobue10 <jlobue10@gmail.com>
2023-01-18 08:26:14 -0800

    Updated systemd service
    Creating a new staging directory for GUI functions as well

--- SteamDeck_rEFInd_install.sh
+++ SteamDeck_rEFInd_install.sh
@@ -52,6 +52,11 @@ yes | sudo cp -rf $CURRENT_WD/icons/ /esp/efi/refind
 # Creating rEFInd EFI entry
 sudo efibootmgr -c -d /dev/nvme0n1 -p 1 -L "rEFInd" -l \\EFI\\refind\\refind_x64.efi
 
+# Granting executable permissions to EFI entry restore script
+chmod +x $CURRENT_WD/restore_EFI_entries.sh
+mkdir -p ~/.SteamDeck_rEFInd/GUI
+cp $CURRENT_WD/restore_EFI_entries.sh ~/.SteamDeck_rEFInd/
+
 # Adding Systemctl daemon for rEFInd to be next boot priority
 # Credit goes to Reddit user lucidludic for the idea :)
 yes | sudo cp $CURRENT_WD/bootnext-refind.service /etc/systemd/system/bootnext-refind.service
@@ -62,8 +67,5 @@ yes | rm ~/deck_passwd_status.txt
 yes | rm ~/efibootlist.txt
 
 sudo steamos-readonly enable
-
-# Granting executable permissions to EFI entry restore script
-chmod +x $CURRENT_WD/restore_EFI_entries.sh
 
 echo -e "\nrEFInd has now been installed (assuming pacman is functional).\n"
