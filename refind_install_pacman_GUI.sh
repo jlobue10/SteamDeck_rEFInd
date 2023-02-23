847093bfcf5b2d559484f116fe0dfeeb428ed73a
jlobue10 <jlobue10@gmail.com>
2023-02-22 19:20:12 -0800

    Update refind_install_pacman_GUI.sh

--- refind_install_pacman_GUI.sh
+++ refind_install_pacman_GUI.sh
@@ -22,15 +22,15 @@
 	echo "# Installing rEFInd package..."
 	sudo pacman -Sy --noconfirm --needed refind
 	sudo refind-install
-	efibootmgr | tee /home/deck/efibootlist.txt
-	WINDOWS_BOOTNUM="$(grep -A0 'Windows' /home/deck/efibootlist.txt | grep -Eo '[0-9]{1,4}' | head -1)"
+	efibootmgr | tee $HOME/efibootlist.txt
+	WINDOWS_BOOTNUM="$(grep -A0 'Windows' $HOME/efibootlist.txt | grep -Eo '[0-9]{1,4}' | head -1)"
 	sudo efibootmgr -b $WINDOWS_BOOTNUM -A
-	REFIND_BOOTNUM="$(grep -A0 'rEFInd Boot Manager' /home/deck/efibootlist.txt | grep -Eo '[0-9]{1,4}' | head -1)"
+	REFIND_BOOTNUM="$(grep -A0 'rEFInd Boot Manager' $HOME/efibootlist.txt | grep -Eo '[0-9]{1,4}' | head -1)"
 	sudo efibootmgr -b $REFIND_BOOTNUM -B
 	echo 50
 	echo "# Fixing EFI entries..."
-	REFIND_BOOTNUM_ALT="$(grep -A0 'rEFInd' /home/deck/efibootlist.txt | grep -Eo '[0-9]{1,4}' | head -1)"
-	STEAMOS_BOOTNUM="$(grep -A0 'SteamOS' /home/deck/efibootlist.txt | grep -Eo '[0-9]{1,4}' | head -1)"
+	REFIND_BOOTNUM_ALT="$(grep -A0 'rEFInd' $HOME/efibootlist.txt | grep -Eo '[0-9]{1,4}' | head -1)"
+	STEAMOS_BOOTNUM="$(grep -A0 'SteamOS' $HOME/efibootlist.txt | grep -Eo '[0-9]{1,4}' | head -1)"
 	re='^[0-9]+$'
 	if [[ $REFIND_BOOTNUM_ALT =~ $re ]]; then
 		sudo efibootmgr -b $REFIND_BOOTNUM_ALT -B
@@ -43,14 +43,14 @@
 	echo "# Installing files to /efi partition..."
 	sudo cp -rf /boot/efi/EFI/refind/ /esp/efi
 	sudo mv /esp/efi/refind/refind.conf /esp/efi/refind/refind-bkp.conf
-	sudo cp /home/deck/.SteamDeck_rEFInd/GUI/refind.conf /esp/efi/refind/refind.conf
-	sudo cp -rf /home/deck/.SteamDeck_rEFInd/backgrounds/ /esp/efi/refind
-	sudo cp -rf /home/deck/.SteamDeck_rEFInd/icons/ /esp/efi/refind
+	sudo cp $HOME/.SteamDeck_rEFInd/GUI/refind.conf /esp/efi/refind/refind.conf
+	sudo cp -rf $HOME/.SteamDeck_rEFInd/backgrounds/ /esp/efi/refind
+	sudo cp -rf $HOME/.SteamDeck_rEFInd/icons/ /esp/efi/refind
 	sudo efibootmgr -c -d /dev/nvme0n1 -p 1 -L "rEFInd" -l \\EFI\\refind\\refind_x64.efi
-	sudo cp /home/deck/.SteamDeck_rEFInd/bootnext-refind.service /etc/systemd/system/bootnext-refind.service
+	sudo cp $HOME/.SteamDeck_rEFInd/bootnext-refind.service /etc/systemd/system/bootnext-refind.service
 	sudo systemctl enable --now bootnext-refind.service
 	sudo efibootmgr -n $(efibootmgr | grep rEFInd | grep -Eo "[0-9]{1,4}" | head -1)
-	rm /home/deck/efibootlist.txt
+	rm $HOME/efibootlist.txt
 	sudo steamos-readonly enable
 	echo 100
 	echo "# Installation completed succesfully."
