bac8ce42d7c0778c689352400c4d5c0a2b9fcb16
jlobue10 <jlobue10@gmail.com>
2023-03-15 00:42:42 -0700

    Installation script updates
    Updated for newer rEFInd version

--- refind_install_no_pacman_GUI.sh
+++ refind_install_no_pacman_GUI.sh
@@ -16,23 +16,20 @@
 	echo 20
 	echo "# Downloading rEFInd zip file..."
 	cd $HOME/Downloads
-	wget https://sourceforge.net/projects/refind/files/0.13.3.1/refind-bin-gnuefi-0.13.3.1.zip
+	wget https://sourceforge.net/projects/refind/files/0.14.0.2/refind-bin-gnuefi-0.14.0.2.zip
 	echo 25
 	echo "# Unzipping rEFInd zip..."
-	unzip -o refind-bin-gnuefi-0.13.3.1.zip
+	unzip -o refind-bin-gnuefi-0.14.0.2.zip
 	sudo steamos-readonly disable
 	sudo mkdir -p /esp/efi/refind
-	yes | sudo cp $HOME/Downloads/refind-bin-0.13.3.1/refind/refind_x64.efi /esp/efi/refind/
-	yes | sudo cp -rf $HOME/Downloads/refind-bin-0.13.3.1/refind/drivers_x64/ /esp/efi/refind
-	yes | sudo cp -rf $HOME/Downloads/refind-bin-0.13.3.1/refind/tools_x64/ /esp/efi/refind
+	yes | sudo cp $HOME/Downloads/refind-bin-0.14.0.2/refind/refind_x64.efi /esp/efi/refind/
+	yes | sudo cp -rf $HOME/Downloads/refind-bin-0.14.0.2/refind/drivers_x64/ /esp/efi/refind
+	yes | sudo cp -rf $HOME/Downloads/refind-bin-0.14.0.2/refind/tools_x64/ /esp/efi/refind
 	echo 50
 	echo "# Installing rEFInd files..."
-	yes | sudo ./refind-bin-0.13.3.1/refind-install
-	yes | sudo cp -rf $HOME/Downloads/refind-bin-0.13.3.1/refind/icons/ /esp/efi/refind
-	yes | sudo cp -rf $HOME/Downloads/refind-bin-0.13.3.1/fonts/ /esp/efi/refind
-	yes | sudo cp $HOME/.SteamDeck_rEFInd/GUI/refind.conf /esp/efi/refind/refind.conf
-	yes | sudo cp -rf $HOME/.SteamDeck_rEFInd/themes/ /esp/efi/refind
-	yes | sudo cp -rf $HOME/.SteamDeck_rEFInd/icons/ /esp/efi/refind
+	yes | sudo ./refind-bin-0.14.0.2/refind-install
+	yes | sudo cp -rf $HOME/Downloads/refind-bin-0.14.0.2/refind/icons/ /esp/efi/refind
+	yes | sudo cp -rf $HOME/Downloads/refind-bin-0.14.0.2/fonts/ /esp/efi/refind
 	efibootmgr | tee $HOME/efibootlist.txt
 	echo 65
 	echo "# Fixing EFI entries..."
