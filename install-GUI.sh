d7aca5ee08faf6246023e0d55596ee922c8bc8c3
jlobue10 <jlobue10@gmail.com>
2023-03-19 11:57:13 -0700

    Update install-GUI.sh

--- install-GUI.sh
+++ install-GUI.sh
@@ -14,7 +14,7 @@ sudo pacman-key --populate archlinux
 # Install base-devel member packages
 sudo pacman -Sy --noconfirm archlinux-keyring autoconf automake binutils bison debugedit fakeroot file findutils flex gawk gcc gettext\
  grep groff gzip libtool m4 make pacman patch pkgconf sed sudo texinfo which
-sudo pacman -Sy --noconfirm glibc hwinfo linux-api-headers qt5-base
+sudo pacman -Sy --noconfirm lib32-glibc glibc hwinfo linux-api-headers qt5-base
 CURRENT_WD=$(pwd)
 mkdir -p $HOME/.SteamDeck_rEFInd/backgrounds
 yes | cp -rf $CURRENT_WD/GUI/ $HOME/.SteamDeck_rEFInd
