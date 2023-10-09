4453cad77ae06326d85676e0ecba840ff7a5b480
jlobue10 <jlobue10@gmail.com>
2023-10-09 11:40:55 -0700

    Delete bios_update_SteamOS_prep.sh

--- bios_update_SteamOS_prep.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# A simple script to prep for SteamOS initiated BIOS updates
-
-sudo systemctl disable --now bootnext-refind.service
-sudo efibootmgr -N
-
-echo -e "\nSteamOS is ready for BIOS updates to occur."
